9e43ff89d63a57bae4ac4e18cda214ed6abefdbe
websupporter <webmaster@websupporter.net>
2018-07-13 14:32:04 +0300

    resolve merge conflict

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,8 @@ const webpack = require( 'webpack' );
 module.exports = {
 	entry: {
 		ingredients:'./assets/js/src/Ingredients/index.js',
-<<<<<<< HEAD
 		editorpicks:'./assets/js/src/EditorPicks/index.js',
-=======
 		featured_image:'./assets/js/src/FeaturedImage/index.js',
->>>>>>> d483dca... add featured_image to webpack
 	},
 	output: {
 		filename:'./assets/js/dist/[name].js'
