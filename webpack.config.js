9f5120b62370e02cf28a8e07b6d4faab1ff77c2e
websupporter <webmaster@websupporter.net>
2018-07-13 14:20:11 +0300

    add featured_image to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require( 'webpack' );
 module.exports = {
 	entry: {
 		ingredients:'./assets/js/src/Ingredients/index.js',
+		featured_image:'./assets/js/src/FeaturedImage/index.js',
 	},
 	output: {
 		filename:'./assets/js/dist/[name].js'
