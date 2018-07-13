c72271a9d540a5db6efdf340e1ee092c7f5822b0
websupporter <webmaster@websupporter.net>
2018-07-13 14:09:39 +0300

    Revert "add editorPicks"
    This reverts commit 22ea4f8e3692b4aa2ddcffe836aad25388465fe4.

--- src/Hooks/CoreHooks.php
+++ src/Hooks/CoreHooks.php
@@ -3,8 +3,6 @@ declare(strict_types = 1);
 
 namespace Inpsyde\Recipe\Hooks;
 
-use Inpsyde\Recipe\Model\EditorPicks;
-use Inpsyde\Recipe\Model\EditorPickMeta;
 use Inpsyde\Recipe\Model\IngredientsBlock;
 use Inpsyde\Recipe\Model\RecipePostType;
 
@@ -27,14 +25,6 @@ class CoreHooks implements Hook
 
                 (new RecipePostType())->register();
                 (new IngredientsBlock($this->rootFile))->register();
-
-                (new EditorPickMeta())->register();
-            }
-        );
-        add_action(
-            'enqueue_block_editor_assets',
-            function () {
-                (new EditorPicks($this->rootFile))->register();
             }
         );
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const webpack = require( 'webpack' );
 module.exports = {
 	entry: {
 		ingredients:'./assets/js/src/Ingredients/index.js',
-		editorpicks:'./assets/js/src/EditorPicks/index.js',
 	},
 	output: {
 		filename:'./assets/js/dist/[name].js'
